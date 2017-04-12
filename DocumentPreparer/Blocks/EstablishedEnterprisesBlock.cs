bbedf447a3ac528c5c0c51c7f13fff26609e7918
aleha84 <astarikh@mail.ru>
2017-04-12 21:46:32 +0300

    ee retrieve

--- DocumentPreparer/Blocks/EstablishedEnterprisesBlock.cs
+++ DocumentPreparer/Blocks/EstablishedEnterprisesBlock.cs
@@ -1,9 +1,11 @@
-﻿using DocumentPreparer.Extensions;
+﻿using DocumentPreparer.Common;
+using DocumentPreparer.Extensions;
 using DocumentPreparer.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace DocumentPreparer.Blocks
 {
@@ -25,10 +27,25 @@ namespace DocumentPreparer.Blocks
                     new EstablishedEnterprise()
                 };
 
-            return new[]
+            var result = Regex.Matches(ee, pattern, RegexOptions.IgnoreCase)
+                .Cast<Match>()
+                .Where(m => m.Success)
+                .Select(m => new EstablishedEnterprise
+                {
+                    Address = CommonHelper.MultiLineProcesser(m.Groups["address"].Value),
+                    INN = m.Groups["inn"].Value,
+                    Name = CommonHelper.MultiLineJoiner(m.Groups["name"].Value),
+                    OGRN = m.Groups["ogrn"].Value,
+                    Share = m.Groups["share"].Value
+                });
+
+            if(!result.Any())
+                return new[]
                 {
                     new EstablishedEnterprise()
                 };
+
+            return result.ToArray();
         }
     }
 }
